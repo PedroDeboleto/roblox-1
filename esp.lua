8cd6a496538f24c07ec467120fc0c887f9c2b44e
LeoKhol <60394659+LeoKholYt@users.noreply.github.com>
2022-02-25 16:34:54 +0200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,15 +1,26 @@
-print('ESP Made by Blissful#4992, edited by LKHUB')
-local Player = game.Players.LocalPlayer
+local module = {}
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
+local S_CG = game:GetService("CoreGui")
 local Camera = workspace.CurrentCamera
-local Mouse = Player:GetMouse()
 
-local function Dist(pointA, pointB) -- magnitude errors for some reason  : (
+local plrsToUPD = {}
+local tracers = false
+
+
+function espFolder()
+    local fold = Instance.new("Folder",S_CG)
+    fold.Name = "LKESP"
+    return fold
+end
+
+function Dist(pointA, pointB)
 	return math.sqrt(math.pow(pointA.X - pointB.X, 2) + math.pow(pointA.Y - pointB.Y, 2))
 end
 
-local function GetClosest(points, dest)
-	local min  = math.huge 
-	local closest = nil
+function GetClosest(points, dest)
+	local closest
+    local min = math.huge
 	for _,v in pairs(points) do
 		local dist = Dist(v, dest)
 		if dist < min then
@@ -20,101 +31,171 @@ local function GetClosest(points, dest)
 	return closest
 end
 
-local function esp(plr)
-	local bilboard = Instance.new("BillboardGui",plr.Character)
-	local text = Instance.new("TextLabel",bilboard)
-	bilboard.Adornee = plr.Character.Head
-	bilboard.StudsOffset = Vector3.new(0,1.5,0)
-	bilboard.Size = UDim2.new(4,25,0.8,10)
-	bilboard.AlwaysOnTop = true
-	bilboard.Active = true
-	bilboard.LightInfluence = 1
-	text.Size = UDim2.new(1,0,1,0)
-	text.BackgroundTransparency = 1
-	text.Text = plr.Name
-	text.Font = Enum.Font.FredokaOne
-	text.TextScaled = true
-	text.TextColor3 = Color3.new(1,1,1)
-	text.TextStrokeTransparency = 0
-	
-	if Drawing ~= nil then
-		local Box = Drawing.new("Quad")
-		Box.Visible = false
-		Box.PointA = Vector2.new(0, 0)
-		Box.PointB = Vector2.new(0, 0)
-		Box.PointC = Vector2.new(0, 0)
-		Box.PointD = Vector2.new(0, 0)
-		Box.Color = Color3.fromRGB(255, 255, 255)
-		Box.Thickness = 2
-		Box.Transparency = 1
+local espFold = S_CG:FindFirstChild("LKESP") or espFolder()
 
-		local function Update()
-			local c
-			c = game:GetService("RunService").RenderStepped:Connect(function()
-				if plr.Character ~= nil and plr.Character:FindFirstChildOfClass("Humanoid") ~= nil and plr.Character:FindFirstChild("HumanoidRootPart") ~= nil and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 and plr.Character:FindFirstChild("Head") ~= nil then
-					local pos, vis = Camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
-					if vis then 
-						local points = {}
-						local c = 0
-						for _,v in pairs(plr.Character:GetChildren()) do
-							if v:IsA("BasePart") then
-								c = c + 1
-								local p = Camera:WorldToViewportPoint(v.Position)
-								if v.Name == "HumanoidRootPart" then
-									p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, 0, -v.Size.Z)).p)
-								elseif v.Name == "Head" then
-									p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, v.Size.Y/2, v.Size.Z/1.25)).p)
-								elseif string.match(v.Name, "Left") then
-									p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(-v.Size.X/2, 0, 0)).p)
-								elseif string.match(v.Name, "Right") then
-									p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(v.Size.X/2, 0, 0)).p)
-								end
-								points[c] = p
-							end
-						end
-						local Left = GetClosest(points, Vector2.new(0, pos.Y))
-						local Right = GetClosest(points, Vector2.new(Camera.ViewportSize.X, pos.Y))
-						local Top = GetClosest(points, Vector2.new(pos.X, 0))
-						local Bottom = GetClosest(points, Vector2.new(pos.X, Camera.ViewportSize.Y))
+function reloadESP()
+    espFold:ClearAllChildren()
+    local function createESP(v)
+        if v.Team == nil or v.Team ~= plr.Team then
+            if Drawing == nil then
+                if espFold:FindFirstChild(v.Name) then espFold:FindFirstChild(v.Name):Destroy() end
+                local esp = Instance.new("BoxHandleAdornment",espFold)
+                esp.Name = v.Name
+                esp.Color3 = v.TeamColor.Color
+                esp.ZIndex = 1
+                esp.Transparency = 0.4
+                esp.Adornee = v.Character
+                esp.Size = v.Character:GetExtentsSize()
+                esp.AlwaysOnTop = true
+                local BillboardGui = Instance.new("BillboardGui",esp)
+                BillboardGui.AlwaysOnTop = true
+                BillboardGui.Adornee = v.Character
+                BillboardGui.Size = UDim2.new(2.5,40,1,5)
+                BillboardGui.StudsOffset = Vector3.new(0,3.5,0)
+                local TextLabel = Instance.new("TextLabel",BillboardGui)
+                TextLabel.BackgroundTransparency = 1
+                TextLabel.Text = v.Name
+                TextLabel.Size = UDim2.new(1,0,1,0)
+                TextLabel.TextScaled = true
+                TextLabel.TextStrokeTransparency = 0.75
+                TextLabel.TextColor3 = Color3.new(1,1,1)
+                TextLabel.Font = Enum.Font.Gotham
+            else
+                local found
+                for _,__ in pairs(plrsToUPD) do
+                    if __.p == v then
+                        found = true
+                    end
+                end
+                if not found then
+                    local Box = Drawing.new("Quad")
+                    Box.Visible = false
+                    Box.PointA = Vector2.new(0, 0)
+                    Box.PointB = Vector2.new(0, 0)
+                    Box.PointC = Vector2.new(0, 0)
+                    Box.PointD = Vector2.new(0, 0)
+                    Box.Color = v.TeamColor.Color
+                    Box.Thickness = 2
+                    Box.Transparency = 1
+                    
+                    local Tracer = Drawing.new("Line")
+                    Tracer.Visible = false
+                    Tracer.Color = v.TeamColor.Color
+                    Tracer.Thickness = 1
+                    Tracer.Transparency = 1
 
-						if Left ~= nil and Right ~= nil and Top ~= nil and Bottom ~= nil then
-							Box.PointA = Vector2.new(Right.X, Top.Y)
-							Box.PointB = Vector2.new(Left.X, Top.Y)
-							Box.PointC = Vector2.new(Left.X, Bottom.Y)
-							Box.PointD = Vector2.new(Right.X, Bottom.Y)
-
-							Box.Visible = true
-						else 
-							Box.Visible = false
-						end
-					else 
-						Box.Visible = false
-					end
-				else
-					Box.Visible = false
-					if game.Players:FindFirstChild(plr.Name) == nil then
-						c:Disconnect()
-					end
-				end
-			end)
-		end
-		coroutine.wrap(Update)()
-	end
+                    table.insert(plrsToUPD,{p=v,b=Box,t=Tracer})
+                end
+            end
+        end  
+    end
+    for _,v in pairs(plrs:GetPlayers()) do
+        if v.Character and v ~= plr and v.Character.Head then
+            createESP(v)
+            v.CharacterAdded:Connect(function()
+                createESP(v)
+            end)
+            
+        end
+    end
 end
 
-for _,v in pairs(game.Players:GetChildren()) do
-	if v.Name ~= Player.Name then
-		pcall(function()
-			esp(v)
-		end)
-	end
-end
+local c
+c = game:GetService("RunService").RenderStepped:Connect(function()
+    for _,v in pairs(plrsToUPD) do
+        local pl = v.p
+        local Box = v.b
+        local Tracer = v.t
 
-game.Players.PlayerAdded:Connect(function(v)
-	v.CharacterAdded:Connect(function()
-		print("LKHUB | ESP |",v,"Added")
-		pcall(function()
-			esp(v)
-		end)
-	end)
+        if pl.Team ~= nil and pl.Team == plr.Team then
+            Box.Visible = false
+            Tracer.Visible = false
+            plrsToUPD[_] = nil
+            continue
+        end
+
+        if pl.Character and pl.Character:FindFirstChildOfClass("Humanoid") and pl.Character:FindFirstChild("HumanoidRootPart") and pl.Character:FindFirstChildOfClass("Humanoid").Health > 0 and pl.Character:FindFirstChild("Head") then
+            local pos, OnScreen = Camera:WorldToViewportPoint(pl.Character.HumanoidRootPart.Position)
+            if OnScreen then 
+                --box esp
+                local points = {}
+                local c = 0
+                for _,v in pairs(pl.Character:GetChildren()) do
+                    if v:IsA("BasePart") then
+                        c = c + 1
+                        local p = Camera:WorldToViewportPoint(v.Position)
+                        if v.Name == "HumanoidRootPart" then
+                            p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, 0, -v.Size.Z)).p)
+                        elseif v.Name == "Head" then
+                            p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, v.Size.Y/2, v.Size.Z/1.25)).p)
+                        elseif string.match(v.Name, "Left") then
+                            p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(-v.Size.X/2, 0, 0)).p)
+                        elseif string.match(v.Name, "Right") then
+                            p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(v.Size.X/2, 0, 0)).p)
+                        end
+                        points[c] = p
+                    end
+                end
+                local Left = GetClosest(points, Vector2.new(0, pos.Y))
+                local Right = GetClosest(points, Vector2.new(Camera.ViewportSize.X, pos.Y))
+                local Top = GetClosest(points, Vector2.new(pos.X, 0))
+                local Bottom = GetClosest(points, Vector2.new(pos.X, Camera.ViewportSize.Y))
+
+                if Left ~= nil and Right ~= nil and Top ~= nil and Bottom ~= nil then
+                    Box.PointA = Vector2.new(Right.X, Top.Y)
+                    Box.PointB = Vector2.new(Left.X, Top.Y)
+                    Box.PointC = Vector2.new(Left.X, Bottom.Y)
+                    Box.PointD = Vector2.new(Right.X, Bottom.Y)
+
+                    Box.Visible = true
+                    Box.Color = pl.TeamColor.Color
+                else 
+                    Box.Visible = false
+                end
+
+                --tracers
+
+                if tracers then
+                    Tracer.From = Vector2.new(Camera.ViewportSize.X/2,Camera.ViewportSize.Y)
+                    Tracer.To = Vector2.new(pos.X,pos.Y)
+                    Tracer.Visible = true
+                end
+            else 
+                Box.Visible = false
+                Tracer.Visible = false
+            end
+        else
+            Box.Visible = false
+            Tracer.Visible = false
+            if not plrs:FindFirstChild(pl) then
+                plrsToUPD[_] = nil
+            end
+        end
+    end
 end)
+
+function module:ESP()
+    print("LKHUB | ESP Loaded")
+    reloadESP()
+
+    plrs.PlayerAdded:Connect(reloadESP)
+    plrs.PlayerRemoving:Connect(reloadESP)
+end
+
+function module:DestroyESP()
+    espFold:Destroy()
+    c:Disconnect()
+    for _,v in pairs(plrsToUPD) do
+        v.b.Visible = false
+    end
+    plrsToUPD = {}
+    print("LKHUB | ESP Unloaded")
+end
+
+function module:Traces(Value)
+    print("LKHUB | Tracers",(Value and "Enabled" or "Disabled"))
+    
+    tracers = Value
+end
+module:ESP()
+return module
